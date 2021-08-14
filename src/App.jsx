26732699165c3285b9b8a4a234cea63bedfe75c9
lanh.hung.son <lanh.hung.son@sun-asterisk.com>
2021-08-14 22:22:28 +0700

    update Login

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 const App = () => {
-  return <Home />
+  return <Register />
 };
 
 export default App;
